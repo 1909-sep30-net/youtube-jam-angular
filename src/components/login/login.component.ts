e72c85d03e75eca314699f7877c2bb31a0149cd0
Javon Negahban <javon.negahban@cox.net>
2019-11-02 17:19:30 -0500

    Added unimplemented login form

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
     selector: 'login',
     templateUrl: './login.component.html',
 })
 export class LoginComponent {
+    user:User = new User('', '', '', '', '');
+
+    constructor(
+        private userService:UserService) {
+    }
+
+    login(form:NgForm) {
+      console.log("login() executes");
+      form.reset();
+    }
 }
