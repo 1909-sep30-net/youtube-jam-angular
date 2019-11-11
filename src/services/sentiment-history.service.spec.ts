83fc86b23fec01fa4fb0899c4d8ab8aa7ba0f829
mariellenolasco <mariellenolasco@gmail.com>
2019-11-11 14:31:13 -0600

    added more tests

--- src/services/sentiment-history.service.spec.ts
+++ src/services/sentiment-history.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule} from '@angular/common/http/testing';
 import { SentimentHistoryService } from './sentiment-history.service';
+import { User } from 'src/models/user';
+import { CommentsSentimentAnalysis } from 'src/models/comments-sentiment-analysis';
+import { YoutubeComment } from 'src/models/youtube-comment';
 
 describe('SentimentHistoryService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -16,4 +19,22 @@ describe('SentimentHistoryService', () => {
     let result = service.getUserSentimentHistory('mtnolasco@up.edu.ph');
     expect(result).toBeTruthy();
   });
+  it('add user sentiment history should add something', () => {
+    const service: SentimentHistoryService = TestBed.get(SentimentHistoryService);
+    let inputUser:User = new User('Marielle', 'Nolasco', 'mtnolasco@up.edu.ph', 'Mathemars');
+    let inputAnalysis : MockCommentAnalysis = new MockCommentAnalysis();
+    let result = service.addUserSentimentHistory(inputUser, inputAnalysis);
+    expect(result).toBeTruthy();
+  })
 });
+class MockCommentAnalysis implements CommentsSentimentAnalysis {
+  averageSentimentScore = 0.5;
+  videoURL = 'hell';
+  analysisDate: Date = new Date();
+  commentList = [new MockYoutubeComment()];
+}
+class MockYoutubeComment implements YoutubeComment {
+  authorName = 'Marielle Nolasco';
+  content = 'Hello World';
+  sentimentScore = 0.97;
+}
